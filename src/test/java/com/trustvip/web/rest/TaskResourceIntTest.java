fe9e46d8afe50117eb70ce3e3086862db0a5dbd4
Ankur Dhawan <adhawan@trustvip.com>
2018-03-02 11:36:11 -0800

    add highlight to article name and link

--- src/test/java/com/trustvip/web/rest/TaskResourceIntTest.java
+++ src/test/java/com/trustvip/web/rest/TaskResourceIntTest.java
@@ -1,15 +1,21 @@
 package com.trustvip.web.rest;
 
-import com.trustvip.VipAdpqApp;
+import static com.trustvip.web.rest.TestUtil.createFormattingConversionService;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.trustvip.domain.Task;
-import com.trustvip.domain.Article;
-import com.trustvip.repository.TaskRepository;
-import com.trustvip.service.TaskService;
-import com.trustvip.repository.search.TaskSearchRepository;
-import com.trustvip.service.dto.TaskDTO;
-import com.trustvip.service.mapper.TaskMapper;
-import com.trustvip.web.rest.errors.ExceptionTranslator;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+
+import javax.persistence.EntityManager;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -25,18 +31,17 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.List;
-
-import static com.trustvip.web.rest.TestUtil.createFormattingConversionService;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.hasItem;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
+import com.trustvip.VipAdpqApp;
+import com.trustvip.domain.Article;
+import com.trustvip.domain.Task;
 import com.trustvip.domain.enumeration.TaskStatus;
+import com.trustvip.repository.TaskRepository;
+import com.trustvip.repository.search.TaskSearchRepository;
+import com.trustvip.service.ArticleService;
+import com.trustvip.service.TaskService;
+import com.trustvip.service.dto.TaskDTO;
+import com.trustvip.service.mapper.TaskMapper;
+import com.trustvip.web.rest.errors.ExceptionTranslator;
 /**
  * Test class for the TaskResource REST controller.
  *
@@ -68,6 +73,9 @@ public class TaskResourceIntTest {
     private TaskService taskService;
 
     @Autowired
+    private ArticleService articleService;
+    
+    @Autowired
     private TaskSearchRepository taskSearchRepository;
 
     @Autowired
@@ -89,7 +97,7 @@ public class TaskResourceIntTest {
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
-        final TaskResource taskResource = new TaskResource(taskService);
+        final TaskResource taskResource = new TaskResource(taskService, articleService);
         this.restTaskMockMvc = MockMvcBuilders.standaloneSetup(taskResource)
             .setCustomArgumentResolvers(pageableArgumentResolver)
             .setControllerAdvice(exceptionTranslator)
