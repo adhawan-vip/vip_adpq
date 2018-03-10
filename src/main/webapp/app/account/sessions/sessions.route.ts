03d92bb57d4b8cd1d9eed859fabb07cbd3a783b1
adhawan325 <dhawan@gmail.com>
2018-03-09 17:42:16 -0800

    Fix routes under account nav

--- src/main/webapp/app/account/sessions/sessions.route.ts
+++ src/main/webapp/app/account/sessions/sessions.route.ts
@@ -7,7 +7,7 @@ export const sessionsRoute: Route = {
     path: 'sessions',
     component: SessionsComponent,
     data: {
-        authorities: ['ROLE_USER'],
+        authorities: ['ROLE_USER', 'ROLE_REVIEWER', 'ROLE_AUTHOR', 'ROLE_ADMIN'],
         pageTitle: 'global.menu.account.sessions'
     },
     canActivate: [UserRouteAccessService]
